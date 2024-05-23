2b76de64a0d531eecb7a442375188975143e4d0f
NobruLcnITS <bruno.luciano@itsrizzoli.it>
2024-05-23 09:14:45 +0200

    rimosso "from utente import * " e sistemato print iniziale per selezione opzione

--- G2_whatsapp_project.py
+++ G2_whatsapp_project.py
@@ -1,6 +1,5 @@
 import redis
 from utente import *
-#from utente import Utente
 #from messaggi import *
 
 def main():
@@ -19,20 +18,19 @@ def main():
     
     print("Benvenuto nel sistema di messaggistica!")
     while True:
-        print("\nSeleziona un'opzione:")
-        print("1. Registrati")
-        print("2. Login")
-        print("3. Esci")
+        print("\nSeleziona un'opzione:\n"+
+              "1. Registrati\n"+
+              "2. Login\n"+
+              "3. Esci\n")
         choice = input("Inserisci il numero dell'opzione: ")
         
         if choice == '1':
             sign_up(r)
-        
         elif choice == '2':
             user_name = login(r)
             if user_name != None: 
                 utente_session(r, user_name)
-        
+
         elif choice == '3':
             print("Grazie per aver utilizzato il sistema di messaggistica. Arrivederci!")
             break
