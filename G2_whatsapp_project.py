494898e0693801c594987a3c44a91b467ea39c8f
NobruLcnITS <bruno.luciano@itsrizzoli.it>
2024-06-01 11:46:46 +0200

    Prova di funzionalità finale

--- G2_whatsapp_project.py
+++ G2_whatsapp_project.py
@@ -3,7 +3,18 @@ from utente import *
 from messaggi import *
 from animazione import *
 
+ # Creare una connessione Redis
+r = redis.Redis(
+host='redis-18510.c55.eu-central-1-1.ec2.redns.redis-cloud.com',
+port=18510,
+db=0,
+username='bruno',
+password='User1234?',
+decode_responses=True,
+)
+    
 def utente_session(r, user_name):
+    
     while True:
         sel = ("\nSeleziona un'opzione:\n" +
             "1. Aggiungi un contatto\n" +
@@ -39,17 +50,137 @@ def utente_session(r, user_name):
         except ValueError:
             clear_screen()
             print('Errore hai inserito un opzione inesistente')
+
+    
+def select_contact_to_chat(r, user_name):
+    contatti = get_friends(r, user_name) 
+    if contatti:
+        print("Utenti trovati:")
+        for idx, contatto in enumerate(contatti, start=1):
+            a = f"{idx}. {contatto}\n"
+            anim(a)
+        try:
+            chat_utente_idx = input("Seleziona un numero per il nome utente del contatto da chat (o digita 'ESC' per uscire): ").strip()
+            if chat_utente_idx.upper() == 'ESC':
+                clear_screen()
+                print("Operazione annullata.")
+                return
             
+            chat_utente_idx = int(chat_utente_idx)
+            if 1 <= chat_utente_idx <= len(contatti):
+                selected_user = contatti[chat_utente_idx - 1]
+                type_chat = input("Che tipologia di chat vuoi iniziare, normale (N) o effimera (E)?: ").strip().upper()
+                # r.sadd(f"chats:{user_name}", chat_utente_idx)
+                # r.sadd(f"chats:{chat_utente_idx}", user_name)
+                if type_chat == 'E':
+                    clear_screen()
+                    print("Chat effimera iniziata.")
+                    chat_session(r, user_name, selected_user, True)
+                else:
+                    clear_screen()
+                    print("Chat iniziata.")
+                    chat_session(r, user_name, selected_user, False)
+            else:
+                print("Numero selezionato non valido.")
+        except ValueError:
+            print('Inserisci un numero valido.')
+    else:
+        print("Non hai contatti in rubrica.")
+
+def active_chats(r, user_name):
+    while True:
+        chats = r.smembers(f"chats:{user_name}")
+        if chats:
+            clear_screen()
+            print(f"\n {Fore.YELLOW}>> chat attive <<{Style.RESET_ALL}\n"+
+                '-'*60)
+            for chat in chats:
+                print(chat)
+            print('-'*60)    
+            chat_user = input("Inserisci il nome utente per continuare la chat o \nscrivi 'ESC' per tornare al menu delle chat: ")
+            if chat_user.upper() == 'ESC':  
+                clear_screen()
+                break
+            if chat_user in chats:
+                chat_session(r, user_name, chat_user,False)
+        else:
+            clear_screen()
+            print(f"\n {Fore.YELLOW} Non hai chat attive {Style.RESET_ALL}")
+            break
+
+def show_chat(r, from_user, to_user, notification: bool):
+    clear_screen()
+    print("\nDigita il tuo messaggio o 'ESC' per tornare al menu delle chat attive: \n" +
+          f"\n {Fore.YELLOW} >> Chat con {to_user} << {Style.RESET_ALL} \n" +
+          '-' * 60)
+    
+    chat = read_messages(r, from_user, to_user)
+    notification_numer = r.zrange(f"notification:{from_user}:{to_user}", 0, -1)
+    
+    # Stampa i messaggi precedenti
+    for msg in chat[:-len(notification_numer)]:
+        print('\n' + msg)
+        
+    # Stampa la notifica e i nuovi messaggi
+    if notification and len(notification_numer) > 0:
+        print(f'\n----------------{Fore.YELLOW} Hai dei nuovi messaggi da {to_user} {Style.RESET_ALL}----------------n')
+        for msg in chat[-len(notification_numer):]:
+            print('\n' + msg)
+    else:
+        for msg in chat:
+            print('\n' + msg)
+                
+                
+    if int(r.hget(f"user:name:{to_user}", "stato")) == 1:
+        print("!! IMPOSSIBILE RECAPIRTARE IL MESSAGIO, L'UTENTE HA LA MODALITA' DND ATTIVA")
+    
+    print('-' * 60)
+
+def delete_notification(r, from_user,to_user):
+    r.delete(f"notification:{from_user}:{to_user}")
+
+def chat_session(r, from_user, to_user, temporary:bool):
+    clear_screen()
+    #subscribe_message(r, from_user,to_user)
+    pubsub = r.pubsub()
+    pubsub.psubscribe(**{f"{from_user}:{to_user}": callback_notification})
+    pubsub.psubscribe(**{f"{to_user}:{from_user}": callback_notification})
+    message = pubsub.get_message()
+    print(message)
+    #r.publish(f"{to_user}:{from_user}", message)
+    thread = pubsub.run_in_thread(sleep_time=0.01)
+    
+    show_chat(r,from_user,to_user,True)    
+    delete_notification(r, from_user,to_user)
+    while True: 
+
+        message = send_message(r, from_user, to_user, temporary)
+        if message is not None:
+            break
+        else:
+            show_chat(r,from_user,to_user,False)
+    thread.stop()
+
+
+def callback_notification(message):
+    # from_user = message['channel'].split(':')[1]
+    # date,to_user,mess = message['data'].split('|',2)
+    print('MY HANDLER: ', message['data'])
+    
+    #r.zadd(f"notification:{to_user}:{from_user}", {f"{date}|{to_user}|{mess}": time.time()})
+    #print(f"{Fore.RED} < {Style.RESET_ALL} {mess} [{date}] (Nuovo messaggio)")
+         
+def subscribe_message(r, from_user,to_user):
+    pubsub = r.pubsub()
+    pubsub.psubscribe(**{f"{from_user}:{to_user}": callback_notification})
+    pubsub.psubscribe(**{f"{to_user}:{from_user}": callback_notification})
+    message = pubsub.get_message()
+    #r.publish(f"{to_user}:{from_user}", message)
+    thread = pubsub.run_in_thread(sleep_time=0.01)
+ 
 def main():
-    # Creare una connessione Redis
-    r = redis.Redis(
-    host='redis-18510.c55.eu-central-1-1.ec2.redns.redis-cloud.com',
-    port=18510,
-    db=0,
-    username='bruno',
-    password='User1234?',
-    decode_responses=True,
-    )
+    global r
+   
     r.ping()
     a = ("\nBenvenuto nel sistema di messaggistica!")
     anim(a)
@@ -80,7 +211,7 @@ def main():
         except ValueError:
             clear_screen()
             print('Errore hai inserito un opzione inesistente')
-            
+
     #r.incr(f"messages:{from_user}:{to_user}")
 if __name__ == '__main__':
     main()
--- messaggi.py
+++ messaggi.py
@@ -1,8 +1,6 @@
 import time
 from animazione import *
 from utente import *
-import threading
-import redis
 
 def send_message(r, from_user, to_user, temporary=False):
          
@@ -21,14 +19,19 @@ def send_message(r, from_user, to_user, temporary=False):
         r.sadd(f"chats:{from_user}", to_user)
         r.sadd(f"chats:{to_user}", from_user)
         
-        r.publish(f"channel:{to_user}", message_data)
+        r.publish(f"{to_user}:{from_user}", message)
+        #r.publish(f"channel:{to_user}", message_data)
+        #print(f"{Fore.RED} < {Style.RESET_ALL} {message} [{timestamp}] (Nuovo messaggio)")
+    
     
     if temporary is True:
         r.expire(f"messages:{from_user}:{to_user}", 60)
         r.expire(f"messages:{to_user}:{from_user}", 60)
     
 def read_messages(r, user_id, chat_id):
-    messages = r.zrange(f"messages:{user_id}:{chat_id}", 0, -1)
+    new_var = f"messages:{user_id}:{chat_id}"
+    messages = r.zrange(new_var, 0, -1)
+    
     formatted_messaged = []
     for message in messages:
         timestamp, sender, msg = message.split("|", 2)
@@ -42,128 +45,9 @@ def delete_messages(r, user_id, chat_id):
     r.srem(f"chats:{user_id}", chat_id)
     r.srem(f"chats:{chat_id}", user_id)
 
-def delete_notification(r, from_user, chat_id):
-    r.delete(f"notification:{from_user}:{chat_id}")
-
 
 def get_status(r,user_name):
     if int(r.hget(f'{hash_name}{user_name}','stato')) == 0:
         return 'False'
     else:
         return 'True'
-    
-def select_contact_to_chat(r, user_name):
-    contatti = get_friends(r, user_name) 
-    if contatti:
-        print("Utenti trovati:")
-        for idx, contatto in enumerate(contatti, start=1):
-            a = f"{idx}. {contatto}\n"
-            anim(a)
-        try:
-            chat_utente_idx = input("Seleziona un numero per il nome utente del contatto da chat (o digita 'ESC' per uscire): ").strip()
-            if chat_utente_idx.upper() == 'ESC':
-                clear_screen()
-                print("Operazione annullata.")
-                return
-
-            chat_utente_idx = int(chat_utente_idx)
-            if 1 <= chat_utente_idx <= len(contatti):
-                selected_user = contatti[chat_utente_idx - 1]
-                type_chat = input("Che tipologia di chat vuoi iniziare, normale (N) o effimera (E)?: ").strip().upper()
-                if type_chat == 'E':
-                    clear_screen()
-                    print("Chat effimera iniziata.")
-                    chat_session(r, user_name, selected_user, True)
-                else:
-                    clear_screen()
-                    print("Chat iniziata.")
-                    chat_session(r, user_name, selected_user, False)
-            else:
-                print("Numero selezionato non valido.")
-        except ValueError:
-            print('Inserisci un numero valido.')
-    else:
-        print("Non hai contatti in rubrica.")
-
-def active_chats(r, user_name):
-    while True:
-        chats = r.smembers(f"chats:{user_name}")
-        if chats:
-            clear_screen()
-            print(f"\n {Fore.YELLOW}>> chat attive <<{Style.RESET_ALL}\n"+
-                '-'*60)
-            for chat in chats:
-                print(chat)
-            print('-'*60)    
-            chat_user = input("Inserisci il nome utente per continuare la chat o \nscrivi 'ESC' per tornare al menu delle chat: ")
-            if chat_user.upper() == 'ESC':  
-                clear_screen()
-                break
-            if chat_user in chats:
-                chat_session(r, user_name, chat_user,False)
-        else:
-            clear_screen()
-            print(f"\n {Fore.YELLOW} Non hai chat attive {Style.RESET_ALL}")
-            break
-
-def notification_number(r, from_user, to_user):
-    keys =  r.zrange(f"notification:{from_user}:{to_user}", 0, -1)
-    return len(keys)
-    
-def show_chat(r, from_user, to_user, notification: bool):
-    clear_screen()
-    print("\nDigita il tuo messaggio o 'ESC' per tornare al menu delle chat attive: \n" +
-          f"\n {Fore.YELLOW} >> Chat con {to_user} << {Style.RESET_ALL} \n" +
-          '-' * 60)
-    
-    chat = read_messages(r, from_user, to_user)
-    count= notification_number(r, from_user, to_user)
-    for i, msg in enumerate(chat):
-    
-        if notification==True and i == len(chat) - count:
-            print(f'\n{Fore.YELLOW}------------------ Hai dei nuovi messagi da {to_user} ------------------{Style.RESET_ALL} \n')
-            print('\n' + msg + " (nuovo messaggio)") 
-        else:
-            print('\n' + msg)    
-    if int(r.hget(f"user:name:{to_user}", "stato")) == 1:
-        print("!! IMPOSSIBILE RECAPIRTARE IL MESSAGIO, L'UTENTE HA LA MODALITA' DND ATTIVA")
-    
-    print('-' * 60)
-    
-def chat_session(r, from_user, to_user, temporary:bool):
-    clear_screen()
-    subscribe_message(r, from_user,to_user)
-    show_chat(r,from_user,to_user,True)    
-    delete_notification(r,from_user,to_user)
-    while True: 
-
-        message = send_message(r, from_user, to_user, temporary)
-        if message is not None:
-            break
-        else:
-            show_chat(r,from_user,to_user,False)
-        
-
-
-def callback_notification(message):
-    r = redis.Redis(
-    host='redis-18510.c55.eu-central-1-1.ec2.redns.redis-cloud.com',
-    port=18510,
-    db=0,
-    username='bruno',
-    password='User1234?',
-    decode_responses=True,  
-    )
-    from_user = message['channel'].split(':')[1]
-    date,to_user,mess = message['data'].split('|',2)
-    r.zadd(f"notification:{from_user}:{to_user}", {f"{date}|{to_user}|{mess}": time.time()})
-    #print(f"{Fore.RED} < {Style.RESET_ALL} {mess} [{date}] (Nuovo messaggio)")
-         
-def subscribe_message(r, from_user,to_user):
-    pubsub = r.pubsub()
-    pubsub.psubscribe(**{f"channel:{from_user}": callback_notification})
-    pubsub.psubscribe(**{f"channel:{to_user}": callback_notification})
-    r.publish("utenti:notification", from_user)
-    pubsub.run_in_thread(sleep_time=0.01)
-
-            
